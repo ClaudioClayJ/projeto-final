43d1cf4764e09309c25747f7d25ee5ad9b4b1965
Claudio Clay <claudio_junior05@hotmail.com>
2024-08-02 12:48:16 -0300

    Barra principal agora some no mobile

--- Frontend/src/pages/componentes/menu/index.js
+++ Frontend/src/pages/componentes/menu/index.js
@@ -1,22 +1,47 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/img/logo.png';
 import { FaFacebookSquare, FaInstagram, FaWhatsapp, FaRegUser } from 'react-icons/fa';
-import { useState } from 'react'; // Importa useState para gerenciar o estado do input
 
 export default function Menu() {
-  const [searchTerm, setSearchTerm] = useState(''); // Estado para armazenar o termo da pesquisa
+  // Estado para armazenar o termo da pesquisa
+  const [searchTerm, setSearchTerm] = useState('');
 
+  // Estado para controlar a visibilidade da barra de navegação ao rolar a página
+  const [scrolling, setScrolling] = useState(false);
+
+  // Função para atualizar o estado com o valor do input de pesquisa
   const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value); // Atualiza o estado com o valor do input
+    setSearchTerm(e.target.value);
   };
 
+  // Função para lidar com a submissão do formulário de pesquisa
   const handleSearchSubmit = (e) => {
-    e.preventDefault(); // Previne o comportamento padrão do formulário
-    console.log('Search term:', searchTerm); // Aqui você pode adicionar a lógica de pesquisa
+    e.preventDefault();
+    console.log('Search term:', searchTerm);
   };
+
+  useEffect(() => {
+    // Função para verificar a rolagem da página e atualizar o estado 'scrolling'
+    const handleScroll = () => {
+      if (window.scrollY > 50) { // Ajuste o valor conforme necessário
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    // Adiciona o listener de rolagem ao componente
+    window.addEventListener('scroll', handleScroll);
+    
+    // Remove o listener de rolagem quando o componente é desmontado
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // Dependência vazia, executa apenas na montagem e desmontagem
 
   return (
-    <div className="container fixed-header">
+    <div className={`container fixed-header ${scrolling ? 'hidden-header' : ''}`}>
       <div className="row">
         <div className="col">
           <img src={logo} alt="Academia" />
@@ -26,26 +51,25 @@ export default function Menu() {
           <h1>Nome Academia</h1>
         </div>
 
+        <div className='col'>
+          <div className="row search-container">
+            <form onSubmit={handleSearchSubmit} className="search-form">
+              <input 
+                type="text" 
+                value={searchTerm} 
+                onChange={handleSearchChange} 
+                placeholder="Buscar..." 
+                className="search-input"
+              />
+              <button type="submit" className="search-button">Buscar</button>
+            </form>
+          </div>
+        </div>
+
         <div className="col icons">
           <FaFacebookSquare size={30} color="white" />
           <FaInstagram size={30} color="white" />
           <FaWhatsapp size={30} color="white" />
-        </div>
-      </div>
-
-      <div className='row'>
-        {/* Barra de busca posicionada logo abaixo dos ícones */}
-        <div className="row search-container">
-          <form onSubmit={handleSearchSubmit} className="search-form">
-            <input 
-              type="text" 
-              value={searchTerm} 
-              onChange={handleSearchChange} 
-              placeholder="Buscar..." 
-              className="search-input"
-            />
-            <button type="submit" className="search-button">Buscar</button>
-          </form>
         </div>
       </div>
 
