46300a848afbdc47f122eebce9ccd0fa99a15cd7
Claudio Clay <claudio_junior05@hotmail.com>
2024-08-06 08:45:14 -0300

    atualizado para a versão House Cj

--- Frontend/src/pages/componentes/menu/index.js
+++ Frontend/src/pages/componentes/menu/index.js
@@ -1,56 +1,99 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/img/logo.png';
 import { FaFacebookSquare, FaInstagram, FaWhatsapp, FaRegUser } from 'react-icons/fa';
+import { IoMenu } from "react-icons/io5";
+import { FaBoxArchive } from "react-icons/fa6";
+import { MdSell } from "react-icons/md";
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Menu() {
-  // Estado para armazenar o termo da pesquisa
   const [searchTerm, setSearchTerm] = useState('');
-
-  // Estado para controlar a visibilidade da barra de navegação ao rolar a página
   const [scrolling, setScrolling] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
+  const menuRef = useRef(null);
 
-  // Função para atualizar o estado com o valor do input de pesquisa
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  // Função para lidar com a submissão do formulário de pesquisa
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log('Search term:', searchTerm);
   };
 
   useEffect(() => {
-    // Função para verificar a rolagem da página e atualizar o estado 'scrolling'
     const handleScroll = () => {
-      if (window.scrollY > 50) { // Ajuste o valor conforme necessário
+      if (window.scrollY > 50) {
         setScrolling(true);
       } else {
         setScrolling(false);
       }
     };
 
-    // Adiciona o listener de rolagem ao componente
     window.addEventListener('scroll', handleScroll);
-    
-    // Remove o listener de rolagem quando o componente é desmontado
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []); // Dependência vazia, executa apenas na montagem e desmontagem
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuVisible(false);
+      }
+    };
+
+    const handleScrollCloseMenu = () => {
+      setMenuVisible(false);
+    };
+
+    if (menuVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+      window.addEventListener('scroll', handleScrollCloseMenu);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener('scroll', handleScrollCloseMenu);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener('scroll', handleScrollCloseMenu);
+    };
+  }, [menuVisible]);
 
   return (
     <div className={`container fixed-header ${scrolling ? 'hidden-header' : ''}`}>
       <div className="row">
         <div className="col">
-          <img src={logo} alt="Academia" />
+          <div className="menu-adm" ref={menuRef}>
+            <IoMenu size={30} onClick={toggleMenu} />
+            <h6 onClick={toggleMenu}>ADMIN</h6>
+            <AnimatePresence>
+              {menuVisible && (
+                <motion.div
+                  className="menu-adm-items"
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.1 }}
+                >
+                  <h4><FaRegUser />Usuarios</h4>
+                  <h4><MdSell />Produtos</h4>
+                  <h4><FaBoxArchive />Estoque</h4>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+          <img className="logo" src={logo} alt="Academia" />
         </div>
-
         <div className="col center">
           <h1>Nome Academia</h1>
         </div>
-
         <div className='col'>
           <div className="row search-container">
             <form onSubmit={handleSearchSubmit} className="search-form">
@@ -65,7 +108,6 @@ export default function Menu() {
             </form>
           </div>
         </div>
-
         <div className="col icons">
           <FaFacebookSquare size={30} color="white" />
           <FaInstagram size={30} color="white" />
@@ -96,4 +138,3 @@ export default function Menu() {
     </div>
   );
 }
-
