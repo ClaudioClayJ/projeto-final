5d0c936f3e6c5714bb52facc88ecc0d5fac0a6f6
Claudio Clay <claudio_junior05@hotmail.com>
2024-08-08 09:14:38 -0300

    cadicionado cards no produto

--- Frontend/src/pages/produtos/index.js
+++ Frontend/src/pages/produtos/index.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import React from "react";
 import '../../global.css'; 
 import Menu from "../componentes/menu";
 
-
 export default function Produtos() {
-return(
-    <div className="dashboard-container">
-        <div className="menu">
-                <Menu/>
+    return (
+        <div className="dashboard-container">
+            <div className="menu">
+                <Menu />
+            </div>
+            <div className="content">
+                <div className="menu-produtos">
+                    {[...Array(3)].map((_, index) => (
+                        <div key={index} className="carousel-produto">
+                            {[...Array(5)].map((_, i) => (
+                                <div key={i} className="card-produto">
+                                    <h3>Produto {index * 5 + i + 1}</h3>
+                                    <p>Descrição do produto {index * 5 + i + 1}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-        <div className="content">
-          
-           </div>
-        </div>
-        
-)
+    );
 }
