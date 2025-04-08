878ed09845d7f89105b55a975af0ebe79358cf9e
Claudio Clay <claudio_junior05@hotmail.com>
2025-04-07 21:45:10 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
-const http = require('http')
-const app = require('./app')
-const port = process.env.port || 5000
-const server = http.createServer(app)
-server.listen(port);
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`🚀 Servidor rodando na porta ${port}`);
+});
